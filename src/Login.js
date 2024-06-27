dd2f6ba2b28d53ca94c489d61b79168488100109
Yash Thakkar <ythakkar246@gmail.com>
2024-06-27 07:19:19 +0530

    initial commit

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+var Login = () =>{
+    const [email,setEmail] = useState();
+    const [pws,setPwd] = useState();
+
+    return (
+        <h1> Login Demo</h1>
+    )
+}
+
+export default Login;
+
